0f1c81df3e74bd739a95ebae3d14b26650bd3b55
eruizgar91 <eruizgar91@gmail.com>
2015-01-20 19:49:23 +0100

    The map is painted.

--- tfg/app/controllers/Application.scala
+++ tfg/app/controllers/Application.scala
@@ -2,6 +2,9 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -14,42 +17,81 @@ import scala.Tuple2;
 
 
 object Application extends Controller {
+  val sc = new SparkContext("local", "Simple App", "/home/eruizgar/play-2.2.2/tfg/app",
+     List("target/scala-2.10/tfg_2.10-1.0-SNAPSHOT.jar"))
+
+     //val data = sc.textFile("carga.csv")
+     val data = sc.textFile("gasolineras-ca-ceres-csv.csv")
+
+     case class Gasolinera (URI: String, om_situadoEnVia: String, rdfs_label: String, om_tieneEnlaceSIG : String, geo_long: String , geo_lat: String)
+  
 
   def index = Action {
 
-		 val sc = new SparkContext("local", "Simple App", "/home/eruizgar/play-2.2.2/tfg/app",
-		 List("target/scala-2.10/tfg_2.10-1.0-SNAPSHOT.jar"))
-
-		 //val data = sc.textFile("carga.csv")
-     val data = sc.textFile("gasolineras-ca-ceres-csv.csv")
-	
+		 
 		
 	val z = data.mapPartitions(lines => {
          val parser = new CSVParser(',')
          lines.map(line => {
            val columns = parser.parseLine(line)
            
-           Array(columns(2),columns(1)).mkString(",")
+           Array(columns(2),columns(1)).mkString("-----")
          })
        })
 
   val l = data.mapPartitions(lines => {
          val parser = new CSVParser(',')
-         lines.map(line => {
+        lines.map(line => {
            val columns = parser.parseLine(line)
-           
            Array(columns(0)).mkString(",")
          })
+
        })
+    var prueba = data.map(_.split(",")).map(
+        r => (r(0), r(1), r(2), r(3), r(4), r(5))
+    ).take(5)
+
 
 	val x = z.collect
   val p = l.collect
 
 	
 
-  Ok(views.html.index(x,p))
+  Ok(views.html.index(x,p,prueba))
 
    // Ok(views.html.index("Your new application is ready."))
   }
+  
+  //NOVEDADES!!!!!
+   val form = Form(
+    single("opcion" -> text)    
+  )
+   //Ahora solo queda hacer una busqueda en función de lo elegido
+  def submit = Action { implicit request =>
+    val fname = form.bindFromRequest
+    val a=fname.get
+
+    var prueba = data.map(_.split(",")).map(
+        r => (r(0),r(1),r(2),r(3),r(4),r(5))
+    ).take(10)
+    
+    //Crear metodo que compare si el valor recibido esta en algun r(0) y devolver todos los datos de la gasolinera elegida.
+   
+   var long=""
+   var lat=""
+   
+   for(i <- 0 until prueba.length){
+    println(prueba(i)._1 + '"' + a +'"')
+    
+    if(prueba(i)._1=='"' + a +'"'){
+      long=prueba(i)._5
+      lat=prueba(i)._6
+    }
+   }
+   
+
+    Ok(views.html.pagina(long,lat))
+  }
+
 
 }
